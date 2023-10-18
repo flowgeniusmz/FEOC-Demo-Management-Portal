79aca7961961f8e9810b53377be2a13ef8c09a0d
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-10-18 09:23:08 -0500

    Create data_FEOC.py

---
+++ functions/data_FEOC.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+def get_FEOC_list():
+    listFEOC = [
+    "FEOC-001",
+    "FEOC-002",
+    "FEOC-003",
+    "FEOC-004",
+    "FEOC-005"
+    ]
+
+    return listFEOC
+
+def get_FEOC_activity():
+    listActivity = [2, 4, 6, 8, 10]
+    return listActivity
